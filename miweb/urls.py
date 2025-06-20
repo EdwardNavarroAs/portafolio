b52cd60004a77bfc08eebbf077a2dd50bb6cf038
EdwardNavarroAs <edwardgrialunicauca@gmail.com>
2025-06-19 19:02:06 -0500

    Agregando la pagina de estudios al proyecto

--- miweb/urls.py
+++ miweb/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('estudios/', views.studies, name='estudios'),
+    path('portafolio/', views.portfolio, name='portafolio'),
 ]
--- miweb/views.py
+++ miweb/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'miweb/home.html')
+
+
+def studies(request):
+    return render(request, 'miweb/studies.html')
+
+def portfolio(request):
+    return render(request, 'miweb/portfolio.html')
